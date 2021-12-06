d103e1fa8caad37969bf96a0fd372be25da809ab
Greet Van der Linden <vanderlindengreet@gmail.com>
2021-12-06 14:59:02 +0100

    comment the main js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,26 @@
 import "../css/index.scss";
-import store from "./data";
-import { getNationalities } from "./data/name";
 import name from "./components/Name";
 import graph from "./components/Graph";
 
 // Intro animation
 
+// animate the header
 const title = document.querySelector("h1");
-
 title.style.animation = "tilt 2s both";
 
+// after the header animation
 title.onanimationend = () => {
+  // animate the intro paragraph
   const introText = document.querySelector(".intro__text");
   introText.style.animation =
     "typing 4s steps(33), blink 0.5s step-end infinite alternate";
   introText.style.width = "33ch";
   introText.style.borderRight = "3px solid";
+  //   when the intro text animation has finished
   introText.onanimationend = () => {
+    //   remove the blinking cursor
     introText.style.borderRight = "none";
-    // Render de name search elements
+    // and after some time, render the form and initialise the Graph component
     setTimeout(() => {
       name(document.getElementById("search"));
       graph(document.getElementById("results"));
