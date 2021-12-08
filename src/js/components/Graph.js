7ecf2645797a696aeee2b33619f300c26bcea65d
Greet Van der Linden <vanderlindengreet@gmail.com>
2021-12-08 07:55:36 +0100

    colored info

--- src/js/components/Graph.js
+++ src/js/components/Graph.js
@@ -81,7 +81,9 @@ class Graph {
       this.holder.insertAdjacentHTML(
         "afterbegin",
         `
-        <p id="likely">Most likely, ${capitalizedValue} is ${
+        <p id="likely" class="${
+          gender === "male" ? "blue" : gender === "female" ? "pink" : "black"
+        }">Most likely, ${capitalizedValue} is ${
           age
             ? gender
               ? "a " + age + " year old " + gender
@@ -101,20 +103,20 @@ class Graph {
               label: "Nationality probability",
               data: probabilities,
               backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
                 "rgba(255, 206, 86, 0.2)",
                 "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
                 "rgba(255, 159, 64, 0.2)",
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
               ],
               borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
                 "rgba(255, 206, 86, 1)",
                 "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
                 "rgba(255, 159, 64, 1)",
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(153, 102, 255, 1)",
               ],
               borderWidth: 1,
             },
